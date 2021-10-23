925ec7b2e28f023146aa121fe9d7b63e5fa6e49a
AngelaTwumasi <74127299+AngelaTwumasi@users.noreply.github.com>
2021-10-23 21:17:35 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCObfpq4RfOYsUYmBKm3Mf6SzCkAFSQe4I",
+    authDomain: "spectagram-app-f1a1c.firebaseapp.com",
+    projectId: "spectagram-app-f1a1c",
+    storageBucket: "spectagram-app-f1a1c.appspot.com",
+    messagingSenderId: "640806475525",
+    appId: "1:640806475525:web:f3e33655b562be58d8496c"
+  };
